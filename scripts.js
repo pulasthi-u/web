9a7e197ce2097b2a0295e75be9bbad3367f60c2b
Pulasthi <pulasthisemail@gmail.com>
2024-07-14 21:20:12 +0530

    kinda cleaned up the css

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@ function nextSection(e) {
     var scrollTop = window.scrollY;
     console.log(scrollTop);
     if (scrollTop < 400) {
-        document.getElementById('main').style.animationName = "moveDown";
+        document.getElementById('home').style.animationName = "moveDown";
 
     } else if ((scrollTop > 400) & (scrollTop < 800)) {
-        document.getElementById('main').style.animationName = "moveUp";
-        document.getElementById('section2').style.animationName = "moveDown";
+        document.getElementById('home').style.animationName = "moveUp";
+        document.getElementById('about').style.animationName = "moveDown";
     } else if (scrollTop > 800) {
-        document.getElementById('section2').style.animationName = "moveUp";
+        document.getElementById('about').style.animationName = "moveUp";
     }
 };
 
