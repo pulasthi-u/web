1855f3f40caa34be454b39d210f344f988236782
unknown <pulasthisemail@gmail.com>
2024-12-27 17:52:01 +0530

    Fixed border around 'see more' in #work. Made the scroll effect work, but would like to try to make it 'nicer' lol

--- scripts.js
+++ scripts.js
@@ -1,13 +1,20 @@
+var prevScroll = 0;
+var scrollBreakpoint = window.innerHeight / 2;
+
 function nextSection(e) {
     var scrollTop = window.scrollY;
-    console.log(scrollTop);
-    if (scrollTop < 400) {
-        document.getElementById('home').style.animationName = "moveDown";
+    var sign = scrollTop - prevScroll;
+    prevScroll = scrollTop;
 
-    } else if ((scrollTop > 400) & (scrollTop < 800)) {
+    // If sign > 0, we are scrolling down.
+
+    if ((scrollTop < scrollBreakpoint) & (sign < 0)) {
+        document.getElementById('home').style.animationName = "moveDown";
+    } else if ((scrollTop > scrollBreakpoint) & (scrollTop < 2 * scrollBreakpoint) & (sign > 0)) {
         document.getElementById('home').style.animationName = "moveUp";
+    } else if ((scrollTop > scrollBreakpoint) & (scrollTop < 2 * scrollBreakpoint) & (sign < 0)) {
         document.getElementById('about').style.animationName = "moveDown";
-    } else if (scrollTop > 800) {
+    } else if ((scrollTop > 2 * scrollBreakpoint) & (sign > 0)) {
         document.getElementById('about').style.animationName = "moveUp";
     }
 };
