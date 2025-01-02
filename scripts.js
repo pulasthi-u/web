1c00c8987f55e7c6eeccef5198f8211826edbdf8
unknown <pulasthisemail@gmail.com>
2025-01-02 11:01:32 +0530

    more css cleanup

--- scripts.js
+++ scripts.js
@@ -3,12 +3,12 @@ var scrollBreakpoint = window.innerHeight / 2;
 
 var workitems = document.getElementById('work-items');
 
-var workscrollbar = document.getElementById('work_scroll');
-var workscrollitems = workscrollbar.getElementsByClassName('scrollitem');
+var workscrollbar = document.getElementById('work-scrollbar');
+var workscrollitems = workscrollbar.getElementsByClassName('scroll-item');
 
 function nextSection(e) {
-    var scrollbar = document.getElementById('scrollbar');
-    var scrollitems = scrollbar.getElementsByClassName('scrollitem');
+    var scrollbar = document.getElementById('main-scrollbar');
+    var scrollitems = scrollbar.getElementsByClassName('scroll-item');
 
     var scrollTop = window.scrollY;
     var sign = scrollTop - prevScroll;
@@ -18,20 +18,20 @@ function nextSection(e) {
 
     if ((scrollTop < scrollBreakpoint) & (sign < 0)) {
         document.getElementById('home').style.animationName = "moveDown";
-        scrollitems[0].className = "scrollitem selected";
-        scrollitems[1].className = "scrollitem";
+        scrollitems[0].className = "scroll-item selected";
+        scrollitems[1].className = "scroll-item";
     } else if ((scrollTop > scrollBreakpoint) & (scrollTop < 2 * scrollBreakpoint) & (sign > 0)) {
         document.getElementById('home').style.animationName = "moveUp";
-        scrollitems[0].className = "scrollitem";
-        scrollitems[1].className = "scrollitem selected";
+        scrollitems[0].className = "scroll-item";
+        scrollitems[1].className = "scroll-item selected";
     } else if ((scrollTop > scrollBreakpoint) & (scrollTop < 2 * scrollBreakpoint) & (sign < 0)) {
         document.getElementById('about').style.animationName = "moveDown";
-        scrollitems[1].className = "scrollitem selected";
-        scrollitems[2].className = "scrollitem";
+        scrollitems[1].className = "scroll-item selected";
+        scrollitems[2].className = "scroll-item";
     } else if ((scrollTop > 2 * scrollBreakpoint) & (sign > 0)) {
         document.getElementById('about').style.animationName = "moveUp";
-        scrollitems[1].className = "scrollitem";
-        scrollitems[2].className = "scrollitem selected";
+        scrollitems[1].className = "scroll-item";
+        scrollitems[2].className = "scroll-item selected";
     }
 };
 
@@ -41,12 +41,11 @@ function showWorkItem(i) {
     workitems.style.transform = 'translateX(-' + (i * 60) + 'vw)';
     for (var j = 0; j < workscrollitems.length; j++) {
         if (j == i) {
-            workscrollitems[j].className = "flex-row vcenter scrollitem selected";
+            workscrollitems[j].className = "flex-row vcenter hcenter left-margin-1_5 scroll-item selected";
         } else {
-            workscrollitems[j].className = "flex-row vcenter scrollitem";
+            workscrollitems[j].className = "flex-row vcenter hcenter left-margin-1_5 scroll-item";
         }
     }
 }
 
-//showWorkItem(0);
 document.addEventListener("scroll", nextSection);
